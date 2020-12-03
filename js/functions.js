09860e86543c2e5198166d8e705e0eda441b8dd7
Diego Barcellos <diegocb81@gmail.com>
2020-12-03 19:39:12 -0300

    Inserindo jQuery e functions

---
+++ js/functions.js
@@ -0,0 +1,7 @@
+/*
+$("body").css("webkit-user-select","auto");
+$("body").css("moz-user-select","auto");
+$("body").css("ms-user-select","auto");
+$("body").css("o-user-select","auto");
+$("body").css("user-select","auto");
+*/
