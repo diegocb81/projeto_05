6ad9d180031a7ea93254fced881c6e2aadf78060
Diego Barcellos <diegocb81@gmail.com>
2020-12-07 11:39:55 -0300

    Atualizando os depoimentos com slides

--- js/functions.js
+++ js/functions.js
@@ -153,4 +153,46 @@ $(function(){
  
     }
 
+    /*
+    Menu responsivo
+    */
+
+    $('.mobile').click(function(){
+        $(this).find('ul').slideToggle();
+    })
+
+    /*
+    Sistema de navegação nos depoimentos da index.html
+    */
+
+    var amtDepoimento = $('.depoimento-single p').length;
+    var curIndex = 0;
+
+    iniciarDepoimentos();
+    navegarDepoimentos();
+
+    function iniciarDepoimentos(){
+        $('.depoimento-single p').hide();
+        $('.depoimento-single p').eq(0).show();
+
+    }
+
+    function navegarDepoimentos(){
+        $('[next]').click(function(){
+            curIndex++;
+            if(curIndex >= amtDepoimento)
+                curIndex = 0;
+                $('.depoimento-single p').hide();
+                $('.depoimento-single p').eq(curIndex).show();
+        })
+
+        $('[prev]').click(function(){
+            curIndex--;
+            if (curIndex < 0)
+                curIndex = amtDepoimento-1;
+            $('.depoimento-single p').hide();
+            $('.depoimento-single p').eq(curIndex).show();
+        })
+    }
+
 })
