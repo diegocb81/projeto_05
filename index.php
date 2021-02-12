9a85b332346c8fbd89df63f4c01b433a85175027
Diego Barcellos <diegocb81@gmail.com>
2021-02-12 19:42:22 -0300

    Adicionando compatibilidade com IE e Edge

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <title>Projeto 05</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=Edge">
     <link rel="stylesheet" href="css/style.css">
     <link rel="shortcut icon" type="image-x/png" href="img/icone.ico">
     <link href="https://fonts.googleapis.com/css?family=Oswald" rel="stylesheet">
